408cc25adfe0f1c94a45811d0ea20408138c8194
blessing.ugavah <bugavah@gmail.com>
2022-07-19 17:29:41 +0100

    this comment modified my code

--- style.js
+++ style.js
@@ -1,8 +1,16 @@
 let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 
 let count = 0
 function increment() {
     count = count + 1
-    countEl.innerText = count 
+    countEl.textContent = count 
+}
+
+function save() {
+    let counStr = count + " - "
+    saveEl.textContent += counStr
+    countEl.textContent = 0
+    count = 0
 }
 
